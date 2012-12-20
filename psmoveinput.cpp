3fc6b1bf76bbfb08421aace5c5977f5528b6cc0c
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2012-12-20 16:07:53 +0400

    fancy version description

--- psmoveinput.cpp
+++ psmoveinput.cpp
@@ -379,7 +379,22 @@ void PSMoveInput::setupSignals()
 
 void PSMoveInput::print_version()
 {
-    std::cout << PSMOVEINPUT_VERSION << std::endl;
+    std::cout << "psmoveinput version " << PSMOVEINPUT_VERSION << "\n";
+    std::cout << "psmoveinput is a Linux input driver turning PSMove controller\n\
+into mouse/keyboard input device\n\n";
+    std::cout << "Copyright (C) 2012  Mikhail Sapozhnikov (masapozhnikov@yandex.ru)\n";
+    std::cout << "This program is free software: you can redistribute it and/or modify\n\
+it under the terms of the GNU General Public License as published by\n\
+the Free Software Foundation, either version 3 of the License, or\n\
+(at your option) any later version.\n\
+\n\
+This program is distributed in the hope that it will be useful,\n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
+GNU General Public License for more details.\n\
+\n\
+You should have received a copy of the GNU General Public License\n\
+along with this program.  If not, see <http://www.gnu.org/licenses/>." << std::endl;
 }
 
 // psmoveinput signal handler
